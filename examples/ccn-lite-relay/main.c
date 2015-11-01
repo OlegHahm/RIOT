5ca6d208f34e00083a2beb689a240f56839389c3
Oleg Hahm <oleg@hobbykeller.org>
2015-11-01 11:20:07 +0900

    SQUASH ME: updated ccn application

--- examples/ccn-lite-relay/main.c
+++ examples/ccn-lite-relay/main.c
@@ -6,14 +6,17 @@
 #include "sys/socket.h"
 #include "arpa/inet.h"
 #include "ccn-lite-riot.h"
+#include "ccnl-core.h"
 #include "ccnl-headers.h"
 #include "ccnl-pkt-ndntlv.h"
 #include "ccnl-defs.h"
+#include "net/gnrc/nettype.h"
 
 #define MAIN_QUEUE_SIZE     (8)
 static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
 //static char addr_str[IPV6_ADDR_MAX_STR_LEN];
-struct ccnl_forward_s fwd;
+
+struct ccnl_if_s *i;
 
 extern struct ccnl_relay_s theRelay;
 
@@ -28,18 +31,16 @@ static const shell_command_t shell_commands[] = {
 
 static void _usage(void)
 {
-    puts("ccnl <prefix> <defaultgw> <UDP port>");
+    puts("ccnl <transport> { <interface> | <defaultgw> <UDP port> }");
 }
 
-static int _ccnl_fwd(int argc, char **argv)
+static int _open_udp_socket(char **argv)
 {
     sockunion sun;
-    int suite = CCNL_SUITE_NDNTLV;
-    char *prefix;
-
-    if (argc < 4) {
-        _usage();
-        return -1;
+    int udpport = NDN_UDP_PORT;
+    if (i->sock < 0) {
+        puts("Something went wrong opening the UDP port");
+        return(-1);
     }
 
     if (inet_pton(AF_INET6, argv[2], &sun.ip6.sin6_addr) != 1) {
@@ -47,14 +48,47 @@ static int _ccnl_fwd(int argc, char **argv)
         return -1;
     }
     sun.ip6.sin6_port = atoi(argv[3]);
+    i->sock = ccnl_open_udpdev(udpport);
 
-    prefix = argv[1];
+    return 0;
+}
 
-    fwd.prefix = ccnl_URItoPrefix(prefix, suite, NULL, NULL);
-    fwd.suite = suite;
-    fwd.face = ccnl_get_face_or_create(&theRelay, 0, &sun.sa, sizeof(sun.ip6));
-    fwd.face->flags |= CCNL_FACE_FLAGS_STATIC;
-    theRelay.fib = &fwd;
+static int _ccnl_fwd(int argc, char **argv)
+{
+    struct ccnl_if_s *i;
+    i = &theRelay.ifs[theRelay.ifcount];
+
+    if (argc < 3) {
+        _usage();
+        return -1;
+    }
+
+    switch (argv[1][0]) {
+        case 'u':
+            if (argc < 4) {
+                _usage();
+                return -1;
+            }
+            if (_open_udp_socket(argv) < 0) {
+                puts("Error opening UDP socket!");
+                return -1;
+            }
+            theRelay.ifcount++;
+            break;
+        case 'i': {
+            int pid = atoi(argv[2]);
+            if (ccnl_open_netif(pid, GNRC_NETTYPE_CCN) < 0) {
+                puts("Error registering at network interface!");
+            }
+            i->if_pid = pid;
+            theRelay.ifcount++;
+            break;
+            }
+        default:
+            _usage();
+            puts("Transport must be either u (UDP) or i (network interface)");
+            return -1;
+    }
 
     ccnl_set_timer(SEC_IN_USEC, ccnl_minimalrelay_ageing, &theRelay, 0);
     ccnl_event_loop(&theRelay);
@@ -66,23 +100,17 @@ static int _ccnl_fwd(int argc, char **argv)
 int main(void)
 {
     msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
-    struct ccnl_if_s *i;
-    int udpport = NDN_UDP_PORT;
 
     puts("Basic CCN-Lite example");
 
     ccnl_core_init();
 
+    /* TODO: use gnrc_netif_get() to get a device for sending */
+
     i = &theRelay.ifs[0];
     i->mtu = NDN_DEFAULT_MTU;
     i->fwdalli = 1;
-    i->sock = ccnl_open_udpdev(udpport);
-    if (i->sock < 0) {
-        puts("Something went wrong opening the UDP port");
-        return(-1);
-    }
     theRelay.ifcount++;
-    printf("NDN minimalrelay started, listening on UDP port %d\n", udpport);
 
     char line_buf[SHELL_DEFAULT_BUFSIZE];
     shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
