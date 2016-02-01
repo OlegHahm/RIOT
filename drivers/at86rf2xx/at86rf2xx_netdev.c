372d3284d9435f65394cfa810078a5155c326823
Thomas Eichinger <thomas.eichinger1@gmail.com>
2016-02-01 17:35:49 +0100

    drivers/at86rf2xx: use basic operation mode instead of advanced

--- drivers/at86rf2xx/at86rf2xx_netdev.c
+++ drivers/at86rf2xx/at86rf2xx_netdev.c
@@ -188,7 +188,7 @@ static int _set_state(at86rf2xx_t *dev, netopt_state_t state)
             at86rf2xx_set_state(dev, AT86RF2XX_STATE_SLEEP);
             break;
         case NETOPT_STATE_IDLE:
-            at86rf2xx_set_state(dev, AT86RF2XX_STATE_RX_AACK_ON);
+            at86rf2xx_set_state(dev, AT86RF2XX_STATE_RX);
             break;
         case NETOPT_STATE_TX:
             if (dev->netdev.flags & AT86RF2XX_OPT_PRELOADING) {
@@ -209,12 +209,12 @@ netopt_state_t _get_state(at86rf2xx_t *dev)
     switch (at86rf2xx_get_status(dev)) {
         case AT86RF2XX_STATE_SLEEP:
             return NETOPT_STATE_SLEEP;
-        case AT86RF2XX_STATE_BUSY_RX_AACK:
+        case AT86RF2XX_STATE_BUSY_RX:
             return NETOPT_STATE_RX;
-        case AT86RF2XX_STATE_BUSY_TX_ARET:
-        case AT86RF2XX_STATE_TX_ARET_ON:
+        case AT86RF2XX_STATE_TX_START:
+        case AT86RF2XX_STATE_PLL_ON:
             return NETOPT_STATE_TX;
-        case AT86RF2XX_STATE_RX_AACK_ON:
+        case AT86RF2XX_STATE_RX:
         default:
             return NETOPT_STATE_IDLE;
     }
@@ -614,16 +614,16 @@ static void _isr(netdev2_t *netdev)
     }
 
     if (irq_mask & AT86RF2XX_IRQ_STATUS_MASK__TRX_END) {
-        if (state == AT86RF2XX_STATE_RX_AACK_ON ||
-            state == AT86RF2XX_STATE_BUSY_RX_AACK) {
+        if(state == AT86RF2XX_STATE_RX ||
+           state == AT86RF2XX_STATE_BUSY_RX) {
             DEBUG("[at86rf2xx] EVT - RX_END\n");
             if (!(dev->netdev.flags & AT86RF2XX_OPT_TELL_RX_END)) {
                 return;
             }
             netdev->event_callback(netdev, NETDEV2_EVENT_RX_COMPLETE, NULL);
         }
-        else if (state == AT86RF2XX_STATE_TX_ARET_ON ||
-                 state == AT86RF2XX_STATE_BUSY_TX_ARET) {
+        else if (state == AT86RF2XX_STATE_PLL_ON ||
+                 state == AT86RF2XX_STATE_TX_START) {
             at86rf2xx_set_state(dev, dev->idle_state);
             DEBUG("[at86rf2xx] EVT - TX_END\n");
             DEBUG("[at86rf2xx] return to state 0x%x\n", dev->idle_state);
