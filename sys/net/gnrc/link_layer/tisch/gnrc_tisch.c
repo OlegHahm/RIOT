e92d49ce51e70c4dcbc3badb56da98f580f067d3
Oleg Hahm <oleg@hobbykeller.org>
2016-03-07 00:34:40 +0100

    tisch: renamed link layer to tisch (was 6tisch)

--- sys/net/gnrc/link_layer/tisch/gnrc_tisch.c
+++ sys/net/gnrc/link_layer/tisch/gnrc_tisch.c
@@ -8,9 +8,9 @@
 
 /**
  * @{
- * @ingroup     net_6tisch
+ * @ingroup     net_tisch
  * @file
- * @brief       Implementation of the 6TISCH MAC protocol
+ * @brief       Implementation of the TISCH MAC protocol
  *
  * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
  * @}
@@ -18,11 +18,14 @@
 
 #include <errno.h>
 
+#include "irq.h"
 #include "kernel.h"
 #include "msg.h"
 #include "thread.h"
-#include "net/gnrc/6tisch.h"
+
+#include "net/gnrc/tisch.h"
 #include "net/gnrc.h"
+
 #include "idmanager.h"
 #include "openqueue.h"
 #include "openrandom.h"
@@ -48,8 +51,8 @@
  */
 static void _event_cb(gnrc_netdev_event_t event, void *data)
 {
-    DEBUG("6tisch: event triggered -> %i\n", event);
-    /* 6TISCH only understands the RX_COMPLETE event... */
+    DEBUG("tisch: event triggered -> %i\n", event);
+    /* TISCH only understands the RX_COMPLETE event... */
     if (event == NETDEV_EVENT_RX_COMPLETE) {
         gnrc_pktsnip_t *pkt;
 
@@ -57,28 +60,28 @@ static void _event_cb(gnrc_netdev_event_t event, void *data)
         pkt = (gnrc_pktsnip_t *)data;
         /* send the packet to everyone interested in it's type */
         if (!gnrc_netapi_dispatch_receive(pkt->type, GNRC_NETREG_DEMUX_CTX_ALL, pkt)) {
-            DEBUG("6tisch: unable to forward packet of type %i\n", pkt->type);
+            DEBUG("tisch: unable to forward packet of type %i\n", pkt->type);
             gnrc_pktbuf_release(pkt);
         }
     }
 }
 
 /**
- * @brief   Startup code and event loop of the 6TISCH layer
+ * @brief   Startup code and event loop of the TISCH layer
  *
  * @param[in] args          expects a pointer to the underlying netdev device
  *
  * @return                  never returns
  */
-static void *_6tisch_thread(void *args)
+static void *_tisch_thread(void *args)
 {
     gnrc_netdev_t *dev = (gnrc_netdev_t *)args;
     gnrc_netapi_opt_t *opt;
     int res;
-    msg_t msg, reply, msg_queue[GNRC_6TISCH_MSG_QUEUE_SIZE];
+    msg_t msg, reply, msg_queue[GNRC_TISCH_MSG_QUEUE_SIZE];
 
     /* setup the MAC layers message queue */
-    msg_init_queue(msg_queue, GNRC_6TISCH_MSG_QUEUE_SIZE);
+    msg_init_queue(msg_queue, GNRC_TISCH_MSG_QUEUE_SIZE);
     /* save the PID to the device descriptor and register the device */
     dev->mac_pid = thread_getpid();
     gnrc_netif_add(dev->mac_pid);
@@ -87,27 +90,27 @@ static void *_6tisch_thread(void *args)
 
     /* start the event loop */
     while (1) {
-        DEBUG("6tisch: waiting for incoming messages\n");
+        DEBUG("tisch: waiting for incoming messages\n");
         msg_receive(&msg);
         /* dispatch NETDEV and NETAPI messages */
         switch (msg.type) {
             case GNRC_NETDEV_MSG_TYPE_EVENT:
-                DEBUG("6tisch: GNRC_NETDEV_MSG_TYPE_EVENT received\n");
+                DEBUG("tisch: GNRC_NETDEV_MSG_TYPE_EVENT received\n");
                 dev->driver->isr_event(dev, msg.content.value);
                 break;
             case GNRC_NETAPI_MSG_TYPE_SND:
-                DEBUG("6tisch: GNRC_NETAPI_MSG_TYPE_SND received\n");
+                DEBUG("tisch: GNRC_NETAPI_MSG_TYPE_SND received\n");
                 dev->driver->send_data(dev, (gnrc_pktsnip_t *)msg.content.ptr);
                 break;
             case GNRC_NETAPI_MSG_TYPE_SET:
                 /* TODO: filter out MAC layer options -> for now forward
                          everything to the device driver */
-                DEBUG("6tisch: GNRC_NETAPI_MSG_TYPE_SET received\n");
+                DEBUG("tisch: GNRC_NETAPI_MSG_TYPE_SET received\n");
                 /* read incoming options */
                 opt = (gnrc_netapi_opt_t *)msg.content.ptr;
                 /* set option for device driver */
                 res = dev->driver->set(dev, opt->opt, opt->data, opt->data_len);
-                DEBUG("6tisch: response of netdev->set: %i\n", res);
+                DEBUG("tisch: response of netdev->set: %i\n", res);
                 /* send reply to calling thread */
                 reply.type = GNRC_NETAPI_MSG_TYPE_ACK;
                 reply.content.value = (uint32_t)res;
@@ -116,19 +119,19 @@ static void *_6tisch_thread(void *args)
             case GNRC_NETAPI_MSG_TYPE_GET:
                 /* TODO: filter out MAC layer options -> for now forward
                          everything to the device driver */
-                DEBUG("6tisch: GNRC_NETAPI_MSG_TYPE_GET received\n");
+                DEBUG("tisch: GNRC_NETAPI_MSG_TYPE_GET received\n");
                 /* read incoming options */
                 opt = (gnrc_netapi_opt_t *)msg.content.ptr;
                 /* get option from device driver */
                 res = dev->driver->get(dev, opt->opt, opt->data, opt->data_len);
-                DEBUG("6tisch: response of netdev->get: %i\n", res);
+                DEBUG("tisch: response of netdev->get: %i\n", res);
                 /* send reply to calling thread */
                 reply.type = GNRC_NETAPI_MSG_TYPE_ACK;
                 reply.content.value = (uint32_t)res;
                 msg_reply(&msg, &reply);
                 break;
             default:
-                DEBUG("6tisch: Unknown command %" PRIu16 "\n", msg.type);
+                DEBUG("tisch: Unknown command %" PRIu16 "\n", msg.type);
                 break;
         }
     }
@@ -136,7 +139,7 @@ static void *_6tisch_thread(void *args)
     return NULL;
 }
 
-kernel_pid_t gnrc_6tisch_init(char *stack, int stacksize, char priority,
+kernel_pid_t gnrc_tisch_init(char *stack, int stacksize, char priority,
                              const char *name, gnrc_netdev_t *dev)
 {
     kernel_pid_t res;
@@ -160,9 +163,9 @@ kernel_pid_t gnrc_6tisch_init(char *stack, int stacksize, char priority,
     sixtop_init();
     neighbors_init();
 
-    /* create new 6TISCH thread */
+    /* create new TISCH thread */
     res = thread_create(stack, stacksize, priority, CREATE_STACKTEST,
-            _6tisch_thread, (void *)dev, name);
+            _tisch_thread, (void *)dev, name);
     if (res <= 0) {
         return -EINVAL;
     }
