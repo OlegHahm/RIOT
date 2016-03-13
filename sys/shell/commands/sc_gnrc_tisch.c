052cc80797c3e5a155858e42b8894973c7563892
Oleg Hahm <oleg@hobbykeller.org>
2016-03-13 23:44:20 +0100

    shell: add command to enable DAGRoot mode

---
+++ sys/shell/commands/sc_gnrc_tisch.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2016 INRIA
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @{
+ *
+ * @file
+ */
+
+#include "idmanager.h"
+
+int _gnrc_tisch_root(int argc, char **argv)
+{
+    (void) argc;
+    (void) argv;
+
+    if (idmanager_getIsDAGroot()) {
+        puts("gnrc_tisch: unset root status of node");
+        idmanager_setIsDAGroot(false);
+    }
+    else {
+        puts("gnrc_tisch: set root status of node");
+        idmanager_setIsDAGroot(true);
+    }
+
+    return 0;
+}
+
+/** @} */
+
