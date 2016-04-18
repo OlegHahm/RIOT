f3bf31a65853e8044e4b48e8798c3a550ed00a0c
Oleg Hahm <oleg@hobbykeller.org>
2016-04-18 08:20:01 +0200

    tisch: fix native ifdef

--- sys/net/gnrc/link_layer/tisch/riot-adaption/radio.c
+++ sys/net/gnrc/link_layer/tisch/riot-adaption/radio.c
@@ -260,7 +260,7 @@ void radio_getReceivedFrame(uint8_t* pBufRead,
         }
         *pLenRead = nread;
 
-#ifndef CPU_NATIVE
+#ifdef CPU_NATIVE
         *pLenRead -= sizeof(ethernet_hdr_t);
         memmove(pBufRead, pBufRead + sizeof(ethernet_hdr_t), *pLenRead);
         *pLqi = rx_info.lqi;
