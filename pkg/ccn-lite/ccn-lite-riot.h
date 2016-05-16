f6fa7dc6f02f8f31c950e3f294c304b261658446
Oleg Hahm <oleg@hobbykeller.org>
2016-05-17 00:10:30 +0200

    WIP: introduce NETOPT_ADDR_SHORT

--- pkg/ccn-lite/ccn-lite-riot.h
+++ pkg/ccn-lite/ccn-lite-riot.h
@@ -154,7 +154,7 @@ static inline void ccnl_free(void *p) {
 /**
  * Size of the message queue of CCN-Lite's event loop
  */
-#define CCNL_QUEUE_SIZE     (8)
+#define CCNL_QUEUE_SIZE     (32)
 
 /**
  * @brief Data structure for interest packet
