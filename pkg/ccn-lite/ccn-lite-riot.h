47217f6beab7f41c87eca244401ecfffc28fdfdf
Oleg Hahm <oleg@hobbykeller.org>
2016-05-18 23:11:36 +0200

    pkg ccn-lite: introduce new NETAPI context

--- pkg/ccn-lite/ccn-lite-riot.h
+++ pkg/ccn-lite/ccn-lite-riot.h
@@ -170,6 +170,7 @@ static inline void ccnl_free(void *p) {
 #define CCNL_CTX_CLEAR_PIT_OWN              (0x0001)
 #define CCNL_CTX_CLEAR_PIT_BUT_OWN          (0x0002)
 #define CCNL_CTX_REMOVE_FIRST_PIT_ENTRY     (0x0003)
+#define CCNL_CTX_PRINT_CS                   (0x0004)
 /** @} */
 
 /**
