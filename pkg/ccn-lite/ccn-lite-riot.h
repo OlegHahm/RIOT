d4a8512dd1635b15402e187d599627616a5f6632
Oleg Hahm <oleg@hobbykeller.org>
2016-05-22 22:33:18 +0200

    pkg ccn-lite: set alternative caching strategies

--- pkg/ccn-lite/ccn-lite-riot.h
+++ pkg/ccn-lite/ccn-lite-riot.h
@@ -322,6 +322,21 @@ void ccnl_set_local_consumer(ccnl_local_func func);
  */
 void ccnl_set_cache_strategy_remove(ccnl_cache_strategy_func func);
 
+/**
+ * @brief Set a function to control the caching strategy
+ *
+ * The given function will be called if the cache is full and a new content
+ * chunk arrives. It shall remove (at least) one entry from the cache.
+ *
+ * If the return value of @p func is 0, the default caching strategy will be
+ * applied by the CCN-lite stack. If the return value is 1, it is assumed that
+ * (at least) one entry has been removed from the cache.
+ *
+ * @param[in] func  The function to be called for an incoming content chunk if
+ *                  the cache is full.
+ */
+void ccnl_set_cache_strategy_remove(ccnl_cache_strategy_func func);
+
 #ifdef __cplusplus
 }
 #endif
