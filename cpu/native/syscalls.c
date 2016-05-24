5dbc7d32df56a1fabea113a6dfce594ac32ff054
Oleg Hahm <oleg@hobbykeller.org>
2016-05-24 11:12:25 +0200

    SM: cpu native: fix merge hickups

--- cpu/native/syscalls.c
+++ cpu/native/syscalls.c
@@ -120,7 +120,6 @@ void _native_syscall_leave(void)
        )
     {
         _native_in_isr = 1;
-        unsigned int mask = irq_disable();
         _native_cur_ctx = (ucontext_t *)sched_active_thread->sp;
         native_isr_context.uc_stack.ss_sp = __isr_stack;
         native_isr_context.uc_stack.ss_size = SIGSTKSZ;
@@ -130,7 +129,6 @@ void _native_syscall_leave(void)
         if (swapcontext(_native_cur_ctx, &native_isr_context) == -1) {
             err(EXIT_FAILURE, "_native_syscall_leave: swapcontext");
         }
-        irq_restore(mask);
     }
 }
 
