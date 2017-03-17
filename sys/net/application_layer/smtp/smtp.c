1ddb61a8d69cc0d8f7a3d35aba53968a82e29e15
Oleg Hahm <oleg@hobbykeller.org>
2017-03-17 15:30:45 +0100

    WIP: SM: smtp updates

--- sys/net/application_layer/smtp/smtp.c
+++ sys/net/application_layer/smtp/smtp.c
@@ -27,9 +27,12 @@
 #include "net/sock.h"
 #include "net/sock/tcp.h"
 
+#define ENABLE_DEBUG    (1)
+#include "debug.h"
+
 sock_tcp_ep_t smtp_mx_relay = { AF_INET6, IPV6_ADDR_UNSPECIFIED, 0, SMTP_DEFAULT_PORT };
 
-static int _smtp_cmd(sock_tcp_t *s, const char *cmd, const size_t len);
+static int _smtp_cmd(sock_tcp_t *sock, const char *cmd, const size_t len, unsigned smtp_code);
 
 void smtp_server_init(sock_tcp_ep_t *s)
 {
@@ -64,21 +67,37 @@ int smtp_sendmail(char *recipient, size_t rlen, char *subject, size_t slen,
         hdr_subject_str,
         ""
     };
+    unsigned smtp_codes[] = {
+        250,
+        250,
+        250,
+        354,
+        0,
+        0,
+        0
+    };
+
     sock_tcp_t sock;
 
     if (sock_tcp_connect(&sock, &smtp_mx_relay, 0, 0) < 0) {
-        puts("Error connecting sock");
+        DEBUG("Error connecting sock\n");
         return 1;
     }
 
+    uint8_t buf[128];
+    if (sock_tcp_read(&sock, buf, sizeof(buf), SOCK_NO_TIMEOUT) < 0) {
+        DEBUG("Disconnected\n");
+    }
+    DEBUG("[smtp] MTA greets: %s\n", buf);
 
     for (unsigned i = 0; i < sizeof(commands) / sizeof(*commands); i++) {
-        if (_smtp_cmd(&sock, commands[i], fmt_strlen(commands[i])) < 0) {
+        if (_smtp_cmd(&sock, commands[i], fmt_strlen(commands[i]), smtp_codes[i]) < 0) {
             return 1;
         }
     }
-    _smtp_cmd(&sock, message, mlen);
-    _smtp_cmd(&sock, ".\n", 3);
+    _smtp_cmd(&sock, message, mlen, 0);
+    _smtp_cmd(&sock, ".\n", 3, 0);
+    _smtp_cmd(&sock, "QUIT\n", 6, 0);
 
     sock_tcp_disconnect(&sock);
     return 0;
@@ -91,22 +110,24 @@ int smtp_add_local_user(char *recipient, smtp_cb_t *cb)
     return 0;
 }
 
-static int _smtp_cmd(sock_tcp_t *sock, const char *cmd, const size_t len)
+static int _smtp_cmd(sock_tcp_t *sock, const char *cmd, const size_t len, unsigned smtp_code)
 {
+    DEBUG("[smtp]: sending %s\n", cmd);
     uint8_t buf[128];
     int res;
-    if (len && ((res = sock_tcp_write(sock, cmd, len)) < 0)) {
-        puts("Errored on write");
+    if (smtp_code && ((res = sock_tcp_write(sock, cmd, len)) < 0)) {
+        DEBUG("Errored on write\n");
         return -1;
     }
     else {
         if ((res = sock_tcp_write(sock, "\n", sizeof("\n"))) < 0) {
-            puts("Errored on write");
+            DEBUG("Errored on write\n");
             return -1;
         }
         if ((res = sock_tcp_read(sock, buf, sizeof(buf), SOCK_NO_TIMEOUT)) < 0) {
-            puts("Disconnected");
+            DEBUG("Disconnected\n");
         }
+        DEBUG("[smtp] Got response: %s\n", buf);
     }
 
     return 0;
